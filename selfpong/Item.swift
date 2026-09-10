30d95711bd12c9145ff79c7d204be5f4d603d047
Aarief <aarieffawwaz@gmail.com>
2026-09-10 09:22:31 +0800

    Initial Commit

---
+++ selfpong/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  selfpong
+//
+//  Created by Aarief Fawwaz Satriahutama on 10/09/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ selfpongTests/selfpongTests.swift
@@ -0,0 +1,19 @@
+//
+//  selfpongTests.swift
+//  selfpongTests
+//
+//  Created by Aarief Fawwaz Satriahutama on 10/09/26.
+//
+
+import Testing
+@testable import selfpong
+
+struct selfpongTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
