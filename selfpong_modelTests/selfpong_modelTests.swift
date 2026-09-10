64200f81f0c91f440e89713c2647f5678b42009a
Aarief <aarieffawwaz@gmail.com>
2026-09-10 09:27:29 +0800

    Initial Commit

---
+++ selfpong_modelTests/selfpong_modelTests.swift
@@ -0,0 +1,19 @@
+//
+//  selfpong_modelTests.swift
+//  selfpong_modelTests
+//
+//  Created by Aarief Fawwaz Satriahutama on 10/09/26.
+//
+
+import Testing
+@testable import selfpong_model
+
+struct selfpong_modelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
